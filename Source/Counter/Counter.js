4c337fb952708b0b697d8ca37a901f36e741203b
Ahmed Raouf <ahmedraouf@Ahmeds-MacBook-Pro.local>
2018-12-29 15:26:31 -0500

    new commits

--- Source/Counter/Counter.js
+++ Source/Counter/Counter.js
@@ -1,12 +1,18 @@
 import React, {Component} from 'react'
-import {Text, View, Button, StyleSheet } from 'react-native'
+import {Text, View, Button, StyleSheet} from 'react-native'
 
 import {connect} from 'react-redux';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 
-import store from '../Redux/Store/store'
-import {Add, Subtract, Reset} from '../Redux/Actions/actions'
+import store from '../Redux/Store/store';
+// import addTodo from '../Redux/Actions/actions';
+
+
+import {bindActionCreators} from 'redux';  
+import { Add, Subtract, Reset } from '../Redux/Actions/actions';
+
+
 
 export class Counter extends Component {
 
@@ -21,7 +27,9 @@ export class Counter extends Component {
             <View>
 
                 <Button onPress={this.props.onAddAction} title="Add"/>
-                <Text style = {styles.counterElement} > {this.props.data} </Text>
+                <Text style={styles.counterElement}>
+                    {this.props.data}
+                </Text>
                 <Button onPress={this.props.onSubtractAction} title="Subtract"/>
                 <Button onPress={this.props.onResetAction} title="Reset"/>
 
@@ -32,27 +40,23 @@ export class Counter extends Component {
 }
 
 const mapStateToProps = (state) => {
-    return {
-        data: state
-    }
+    return {data: state}
 }
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        onAddAction: () => {
-            dispatch({type: 'ADD'})
-        },
-        onSubtractAction: () => {
-            dispatch({type: 'Subtract'})
-        },
-        onResetAction: () => {
-            dispatch({type: 'Reset'})
-        }
-    }
+    return bindActionCreators({
+        onAddAction      : Add,
+        onSubtractAction : Subtract,
+        onResetAction: Reset
+      }, dispatch);
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Counter);
 
-const styles = StyleSheet.create({ 
-    counterElement:{ fontSize:40 }
- })
+const styles = StyleSheet.create({
+    counterElement: {
+
+        fontSize: 40,
+        color: 'green'
+    }
+})
--- Source/Redux/Actions/actions.js
+++ Source/Redux/Actions/actions.js
@@ -1,8 +1,17 @@
-function Add  (text) {
-    type:'ADD',
-    text
-}
-export default Add;
+import { createAction } from 'redux-actions';
+
+export const Add      =  createAction("ADD");
+export const Subtract =  createAction("Subtract");
+export const Reset    =  createAction("Reset")
+
+
+
+// function addTodo() {
+//     return {
+//       type: "ADD"
+//     }
+//   }
+// export default addTodo;
 // export const Minus = () => {
 //     type:'Minus'
 // }
