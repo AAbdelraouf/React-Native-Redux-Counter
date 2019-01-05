226f0a81b1e739dd63ae0e0ac7eb4b9f519786a9
Ahmed Raouf <ahmedraouf@Ahmeds-MBP.home>
2019-01-04 18:58:00 -0800

    new commits

--- App.js
+++ App.js
@@ -1,24 +1,18 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
+import {Platform, StyleSheet, Text, View} from 'react-native';=
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 import store from './Source/Redux/Store/store'
-
 import Counter from './Source/Counter/Counter'
 
 class App extends Component {
     render() {
         return (
-
             <Provider store={store}>
-
                 <View style={styles.container}>
                     <Text style={styles.welcome}>Welcome to React Native!</Text>
                     <Counter/>
-
                 </View>
-
             </Provider>
 
         );
