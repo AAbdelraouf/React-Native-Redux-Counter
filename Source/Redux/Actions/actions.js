8c4911dc28141b0203ecfd2c03845eecda90d136
Ahmed Raouf <ahmedraouf@Ahmeds-MBP.home>
2019-01-04 18:51:20 -0800

    new commits

--- Source/Redux/Actions/actions.js
+++ Source/Redux/Actions/actions.js
@@ -2,4 +2,4 @@ import {createAction} from 'redux-actions';
 
 export const Add = createAction("ADD");
 export const Subtract = createAction("Subtract");
-export const Reset = createAction("Reset")
+export const Reset = createAction("Reset");
--- Source/Redux/Reducers/reducer.js
+++ Source/Redux/Reducers/reducer.js
@@ -1,7 +1,7 @@
 
 const reducer = (state, action) => {
     switch(action.type){
-        case 'ADD'   : return state + 1 
+        case 'ADD'   : return state + 1
         case "Subtract" : return state -1 
         case 'Reset' : return state = 0
         default: return state
